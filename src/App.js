42ec94c34961881739505303da1d38814b3f6cce
Ella91 <75183157+Ella91@users.noreply.github.com>
2021-02-28 13:09:36 +0100

    Added Github-Link in the footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import React from "react";
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+  <div className="App">
+    <h1>Weather App</h1>
+      <footer>
+        This site was coded by Ella and is {" "}
+        <a 
+        href="https://github.com/Ella91/weather-react-project" target="_blank">
+        open-sourced on GitHub</a>
+      </footer> 
+    </div>
+  );
 }
 
